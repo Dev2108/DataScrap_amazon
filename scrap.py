9dd0c1677bf00f17e89d540e0696dd7bb81120ab
Prashant Tripathi <66847426+Dev2108@users.noreply.github.com>
2021-02-02 19:59:52 +0530

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -1,6 +1,9 @@
 import requests
+import logging
 from openpyxl.workbook import Workbook
 from bs4 import BeautifulSoup
+from openpyxl.styles import PatternFill
+
 speakers=[]
 for i in range(1,16):
     link="https://www.amazon.in/s?k=top+50+speakers&page="+str(i)+"&qid=1612250144&ref=sr_pg_"+str(i)
@@ -17,7 +20,9 @@ for i in range(1,16):
         name=d.find('span', attrs={'class':'a-size-medium a-color-base a-text-normal' }).text
   
         if d.find('span', attrs={'class':'a-price-whole'}):
-            Listed_price=d.find('span', attrs={'class':'a-price-whole'}).text
+            s=d.find('span', attrs={'class':'a-price-symbol'}).text
+            l=d.find('span', attrs={'class':'a-price-whole'}).text
+            Listed_price= str(s)+str(l)
         else:
             Listed_price="NA"
         if d.find('span', attrs={'class':'a-price a-text-price'}):
@@ -27,28 +32,43 @@ for i in range(1,16):
             else:
                 Actual_price="NA"
         if d.find('span', attrs={'class':'a-icon-alt'}):
-            rating=d.find('span', attrs={'class':'a-icon-alt'}).text
+            rating=d.find('span', attrs={'class':'a-icon-alt'}).text[0:3]
         else:
             rating="NA"
         speakers.append([name,Listed_price,Actual_price,rating])
                  
 
-       
+        import pandas as pd
+        df= pd.DataFrame()
+        df['speakers']=speakers
 
-
-
+print(df)
 
 
 wb=Workbook()
     # grab the active worksheet
 ws = wb.active
     # Data can be assigned directly to cells
+ws['A1'].fill = PatternFill(start_color="0000CCFF", fill_type = "solid")
 ws['A1'] = 'Name'
+ws['B1'].fill = PatternFill(start_color="0000CCFF", fill_type = "solid")
 ws['B1'] = 'Listing Price'
-
+ws['c1'].fill = PatternFill(start_color="0000CCFF", fill_type = "solid")
 ws['C1'] = 'Actual Price'
+ws['D1'].fill = PatternFill(start_color="0000CCFF", fill_type = "solid")
 ws['D1'] = 'Ratings'
 for i in speakers:
     ws.append(i)
   # Save the file
 wb.save("amazon_top_speakers.xlsx")
+
+log_records=[]
+for i in speakers:
+    log_records.append(i)
+    logging.info(i)
+wb_log=Workbook()
+ws_log=wb_log.active
+ws_log['A1']='Logging_Info'
+for logs in log_records:
+    ws_log.append(logs)
+wb_log.save("Logging Report.xlsx")
